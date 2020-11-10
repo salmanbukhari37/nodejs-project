2f2fcf738e31330530fd364ed76a19cc3b17dde2
Salman Bukhari <salman@renegadefurniture.com>
2020-11-11 01:26:54 +0500

    express routes

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+
 const app = express();
 
+// PUT, POST, GET, DELETE
 
-app.get('/get-data', (req, res) => {
-    // fetch db data
-    const dbData = {
-        name: "salman",
-        age: 29,
-        hobbies: ["programming"]
-    }
+const carArr = [
+    {id: 1, name: "Hyundai Santa FE"},
+    {id: 2, name: "Mercedes Benz"},
+    {id: 3, name: "Toyota Yaris"}
+]
 
-    res.send(dbData);
+// const authorization = (req, res, next) => {
+//     console.log("authorization occured");
+//     next();
+// }
+
+// app.use(authorization)
+// app.use(bodyParser.urlencoded({extended: true}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/cars', (req, res) => {
+    res.send(carArr);
 })
 
-app.get('/get-cars', (req, res) => {
-    const dbCarData = {
-        name: "Honda Civic",
-        color: "Black",
-        transmission: "automatic"
+app.get('/cars/:id', (req, res) => {
+    // console.log(req.params);
+    const filteredData = carArr.filter(({id}) => id === parseInt( req.params.id ) );
+    // console.log(filteredData);
+    if (filteredData.length > 0) {
+        res.send(filteredData);
+    }else{
+        res.status(400).send("No cars found");
     }
-
-    res.send(dbCarData);
 })
+
+app.post("/cars", (req, res) => {
+
+    carArr.push({
+        id: carArr.length + 1,
+        name: req.body.name
+    });
+    // console.log(carArr);
+
+    res.send(carArr);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
